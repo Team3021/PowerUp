99bacca75f877780708d6b0645e1654da6cf878f
Hawrysh, Trevor <Trevor.Hawrysh@thermofisher.com>
2018-02-17 21:27:28 -0800

    change joystick buttons to be unmapped to a method

--- src/org/usfirst/frc/team3021/robot/controller/station/AttackThreeController.java
+++ src/org/usfirst/frc/team3021/robot/controller/station/AttackThreeController.java
@@ -13,20 +13,20 @@ public class AttackThreeController extends BaseController {
 	}
 
 	private void setButtons() {
-		buttonActions.add(new ButtonAction(1, "STICK_TRIGGER", "isLaunching"));
-		buttonActions.add(new ButtonAction(2, "STICK_MIDDLE_BUTTON", "isRotatingToOneHundredEighty"));
-		buttonActions.add(new ButtonAction(3, "STICK_CENTER", "isCollecting"));
-		buttonActions.add(new ButtonAction(4, "STICK_LEFT_BUTTON", "isRotatingToNegativeNinety"));
-		buttonActions.add(new ButtonAction(5, "STICK_RIGHT_BUTTON", "isRotatingToNinety"));
+		buttonActions.add(new ButtonAction(1, "STICK_TRIGGER", "Not Assigned"));
+		buttonActions.add(new ButtonAction(2, "STICK_MIDDLE_BUTTON", "Not Assigned"));
+		buttonActions.add(new ButtonAction(3, "STICK_CENTER", "Not Assigned"));
+		buttonActions.add(new ButtonAction(4, "STICK_LEFT_BUTTON", "Not Assigned"));
+		buttonActions.add(new ButtonAction(5, "STICK_RIGHT_BUTTON", "Not Assigned"));
 	
-		buttonActions.add(new ButtonAction(6, "BASE_LEFT_FRONT", "isRotatingLeft45"));
+		buttonActions.add(new ButtonAction(6, "BASE_LEFT_FRONT", "Not Assigned"));
 		buttonActions.add(new ButtonAction(7, "BASE_LEFT_BACK", "isZeroGyro"));
 	
-		buttonActions.add(new ButtonAction(8, "BASE_BOTTOM_LEFT", "isSwitchingCamera"));
+		buttonActions.add(new ButtonAction(8, "BASE_BOTTOM_LEFT", "Not Assigned"));
 		buttonActions.add(new ButtonAction(9, "BASE_BOTTOM_RIGHT", "isStoppingCommands"));
 		
 		buttonActions.add(new ButtonAction(10, "BASE_RIGHT_BACK", "isZeroEncoders"));
-		buttonActions.add(new ButtonAction(11, "BASE_RIGHT_FRONT", "isRotatingRight45"));
+		buttonActions.add(new ButtonAction(11, "BASE_RIGHT_FRONT", "Not Assigned"));
 	}
 	
 	@Override
