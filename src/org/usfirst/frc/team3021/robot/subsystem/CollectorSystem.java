89ae1d951fe568259f72fabd469d8695dab2b31b
Agency3021 <Agency3021@LAPTOP-P8LGCAKB>
2018-02-20 16:36:01 -0800

    Fixed button so LED lights up

--- src/org/usfirst/frc/team3021/robot/subsystem/CollectorSystem.java
+++ src/org/usfirst/frc/team3021/robot/subsystem/CollectorSystem.java
@@ -7,6 +7,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 
 public class CollectorSystem extends Subsystem {
 	
@@ -16,11 +18,14 @@ public class CollectorSystem extends Subsystem {
 	private boolean isEnabled = ENABLED_DEFAULT;
 
 	private static final String PREF_VOLTAGE = "Collector.motor.voltage";
-	private static final double VOLTAGE_DEFAULT = 0.55;
+	private static final double VOLTAGE_DEFAULT = 0.65;
 
 	private double voltage = VOLTAGE_DEFAULT;
 	
 	private static final double REVERSE_MULTIPLIER = -1.0;
+	
+	private static DigitalInput tote_switch;
+	private static Relay        tote_light;
 	
 	private static Compressor compressor;
 
@@ -44,6 +49,9 @@ public class CollectorSystem extends Subsystem {
 			left_motor = new WPI_TalonSRX(27);
 			left_motor.setInverted(true);
 			
+			tote_switch = new DigitalInput(4);
+			tote_light = new Relay(0);        ;
+
 			compressor = new Compressor(0);
 			compressor.setClosedLoopControl(true);
 		}
@@ -55,29 +63,39 @@ public class CollectorSystem extends Subsystem {
 		if (!isEnabled) {
 			return;
 		}
+		else{
+			
+		}
+		
+		if (tote_switch.get()) {
+			tote_light.set(Relay.Value.kOff);
+		}
+		else {
+			tote_light.set(Relay.Value.kForward);
+	    }			
 
 		if (auxController.isClimberSafteyOn())  {
 			collector_deploy.set(false);
 			collector_stow.set(true);	
 		}
 		else {
-			if (!collecter_deployed && auxController.isCollectorDeploying()) {
+			if (auxController.isCollectorDeploying()) {
 				collector_deploy.set(true);
 				collector_stow.set(false);
 				collecter_deployed = true;
 			}
 			
-			if (collecter_deployed && auxController.isCollectorStowing()) {
+			if (auxController.isCollectorStowing()) {
 				collector_deploy.set(false);
 				collector_stow.set(true);	
 				collecter_deployed = false;
 			}
 
 			// Control the motor
-			if (collecter_deployed && (mainController.isCollecting() || auxController.isCollecting())) {
+			if (mainController.isCollecting() || auxController.isCollecting()) {
 				startMotor();
 			}
-			else if (collecter_deployed && (mainController.isLaunching() || auxController.isLaunching())) {
+			else if (mainController.isLaunching() || auxController.isLaunching()) {
 				reverseMotor();
 			}
 			else {
@@ -87,12 +105,12 @@ public class CollectorSystem extends Subsystem {
 	}
 
 	public void startMotor() {
-		right_motor.set(getVoltage());
+		right_motor.set(REVERSE_MULTIPLIER * getVoltage());
 		left_motor.set(getVoltage());
 	}
 	
 	public void reverseMotor() {
-		right_motor.set(REVERSE_MULTIPLIER * getVoltage());
+		right_motor.set(getVoltage());
 		left_motor.set(REVERSE_MULTIPLIER * getVoltage());
 	}
 	
